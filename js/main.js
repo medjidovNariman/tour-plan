35b9c4789639eb674b7304a0f6ea0a5cd6890b74
nariman <nariman.miedzhidov@mail.ru>
2022-01-02 23:31:38 +0300

    create: swiper-slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
