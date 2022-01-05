101a25f0d6f898bc84cd32ba0a3c13128e3e9676
nariman <nariman.miedzhidov@mail.ru>
2022-01-05 21:13:23 +0300

    create section reviews

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
   loop: true,
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
   scrollbar: {
     el: '.swiper-scrollbar',
+  },
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  loop: true,
+  navigation: {
+    nextEl: '.reviews-slider__button--prev',
+    prevEl: '.reviews-slider__button--next',
   },
 });
